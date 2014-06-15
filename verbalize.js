308e10bba8eb387f8a424672c833e09f201f2d88
Daniel Hug <danielthehug@gmail.com>
2014-06-15 16:52:25 -0700

    rename global: verbalize -> vb

--- verbalize.js
+++ verbalize.js
@@ -7,32 +7,32 @@
 		define([], factory);
 	else if (typeof exports === 'object')
 		module.exports = factory();
-	else root.verbalize = factory();
+	else root.vb = factory();
 })(this, function () {
-	var verbalize = {};
+	var vb = {};
 
 
 	// Capitalize the first character in a string:
-	verbalize.capitalize = function(str) {
+	vb.capitalize = function(str) {
 		return str.length ? str[0].toUpperCase() + str.slice(1) : str;
 	};
 
 
 	//	Accepts an integer and an optional verb:
-	//		verbalize.times(1, 'try')
+	//		vb.times(1, 'try')
 	//	Returns the number of times paired with the verb:
 	//		"try once"
-	verbalize.times = function(num, verb) {
+	vb.times = function(num, verb) {
 		var adv = ['never', 'once', 'twice'][num] || num + ' times';
 		return verb ? (num ? verb + ' ' + adv : adv + ' ' + verb) : adv;
 	};
 
 
 	//	Accepts an array of items, and an optional coordinating conjunction ("and" is default):
-	//		verbalize.list(['apples', 'oranges', 'bananas'])
+	//		vb.list(['apples', 'oranges', 'bananas'])
 	//	Returns a comma-delimited list with the conjunction before the last item if there are 3 or more:
 	//		"apples, oranges, and bananas"
-	verbalize.list = function(items, cunjunction) {
+	vb.list = function(items, cunjunction) {
 		cunjunction = ' ' + (cunjunction || 'and') + ' ';
 		if (items.length < 3) return items.join(cunjunction);
 		return items.slice(0, -1).join(', ') + ',' + cunjunction + items[items.length - 1];
@@ -40,10 +40,10 @@
 
 
 	//	Accepts an integer:
-	//		verbalize.order(312)
+	//		vb.order(312)
 	//	Returns the integer's ordinal number:
 	//		"312th"
-	verbalize.order = function(num) {
+	vb.order = function(num) {
 		var mod100 = num % 100;
 		var suffix = mod100 > 4 && mod100 < 21 ? 0 : ['st', 'nd', 'rd'][num % 10 - 1];
 		return num + (suffix || 'th');
@@ -51,21 +51,21 @@
 
 
 	//	Accepts a singular noun:
-	//		verbalize.aOrAn('apple')
+	//		vb.aOrAn('apple')
 	//	Returns the noun paired with the correct article:
 	//		"an apple"
 	//	false positives: "hour", "university", "FBI agent"
-	verbalize.aOrAn = function(noun) {
+	vb.aOrAn = function(noun) {
 		return ('aeiouAEIOU'.indexOf(noun[0]) >= 0 ? 'an ' : 'a ') + noun;
 	};
 
 
 	//	Accepts a singular noun:
-	//		verbalize.plural('box')
+	//		vb.plural('box')
 	//	Returns the noun in plural form:
 	//		"boxes"
 	//	false positives: "ox", "potato", "half", "deer", etc.
-	verbalize.plural = function(noun) {
+	vb.plural = function(noun) {
 		var lastChar = noun.slice(-1);
 		if ('sxz'.indexOf(lastChar) >= 0 ||
 			['ch', 'sh'].indexOf(noun.slice(-2)) >= 0) return noun + 'es';
@@ -74,16 +74,16 @@
 	};
 
 
-	//	Accepts a number, a singular noun, and an optional plural noun (plural is inferred using verbalize.plural if not given):
-	//		verbalize.count(3, 'apple')
+	//	Accepts a number, a singular noun, and an optional plural noun (plural is inferred using vb.plural if not given):
+	//		vb.count(3, 'apple')
 	//	Returns the number paired with the correct noun:
 	//		"3 apples"
-	verbalize.count = function(num, singular, plural) {
-		plural = plural || verbalize.plural(singular);
+	vb.count = function(num, singular, plural) {
+		plural = plural || vb.plural(singular);
 		var noun = num === 1 ? singular : plural;
 		return num + ' ' + noun;
 	};
 
 
-	return verbalize;
+	return vb;
 });
