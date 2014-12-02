983d259edecfc31ba215a57e2de6e38503e61d9e
Daniel Hug <danielthehug@gmail.com>
2014-12-01 17:26:34 -0800

    vb.plural: more comments, handle wolf, etc.

--- verbalize.js
+++ verbalize.js
@@ -86,12 +86,28 @@
 	//		vb.plural('box')
 	//	Returns the noun in plural form:
 	//		"boxes"
-	//	false positives: "ox", "potato", "half", "deer", etc.
+	//	false positives: "ox", "potato", "goose", "deer", etc.
 	vb.plural = function(noun) {
 		var lastChar = noun.slice(-1);
+		var last2;
+
+		// if ends with 's', 'x', 'z', 'ch', or 'sh', add 'es'
 		if ('sxz'.indexOf(lastChar) >= 0 ||
-			['ch', 'sh'].indexOf(noun.slice(-2)) >= 0) return noun + 'es';
+			['ch', 'sh'].indexOf(last2 = noun.slice(-2)) >= 0) return noun + 'es';
+
+		// else, if ends with 'y', replace with 'ies'
 		if (lastChar === 'y') return noun.slice(0, -1) + 'ies';
+
+		// else, if ends with 'fe' or 'lf', replace f+ with 'ves'
+		if (['fe', 'lf'].indexOf(last2) >= 0) {
+			var fi = noun.lastIndexOf('f');
+			return noun.slice(0, fi) + 'ves';
+		}
+
+		// else, if ends with 'man' replace with 'men'
+		if (noun.slice(-3) === 'man') return noun.slice(0, -3) + 'men';
+
+		// else add 's'
 		return noun + 's';
 	};
 
